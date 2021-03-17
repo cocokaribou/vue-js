a68ab4a6a60ed8b7831d97d629b113c5735b3f12
Youngin Lee <yilee@directcloud.co.jp>
2021-03-17 17:23:13 +0900

    Modified 'index.html' file

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data:{
         product: 'Socks',
         description: 'warm and cozy',
-        image: './resources/img.jpg',
-        link: 'https://www.naver.com',
+        image: './resources/vmSocks-green.jpg',
+        link: 'https://velog.io/@cocokaribou',
         inventory: 100,
         onSale: true,
         details:["80% cotton", "20% polyster", "Gender-neutral "],
         variants:[
             {
                 variantId:2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: './resources/vmSocks-green.jpg'
             },
             {
                 variantId:2235,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: './resources/vmSocks-blue.jpg'
             }
         ],
         sizeArr:[
@@ -34,7 +36,22 @@ var app = new Vue({
         ],
         cart: 0
     },
-    functions:{
-        sayhello(){}
+    methods:{
+        addToCart: function(){
+            this.cart += 1
+        },
+        removeFromCart: function(){
+            this.cart -= 1
+            if(this.cart <= 0) {
+                this.cart = 0
+            } 
+        },
+        updateProduct: function(variantImage){
+            this.image = variantImage;
+        },
+        //updateProduct(variantImage){... }  not all browsers support this feature
+        ohhey(){
+
+        }
     }
 })
