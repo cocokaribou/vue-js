d65a4e1f684ba3c9bb8605d2ba36906c9bd1a1a5
Youngin Lee <yilee@directcloud.co.jp>
2021-03-17 15:06:46 +0900

    Added files
    index.html
    main.js
    style.css

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        product: 'Socks',
+        description: 'warm and cozy',
+        image: './resources/img.jpg',
+        link: 'https://www.naver.com'
+    },
+    functions:{
+        sayhello(){}
+    }
+})
