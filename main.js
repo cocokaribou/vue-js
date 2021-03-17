91e899f21e6fa12cf32fd745f8cc308fb2991887
Youngin Lee <yilee@directcloud.co.jp>
2021-03-17 16:01:08 +0900

    Added files

--- main.js
+++ main.js
@@ -4,7 +4,35 @@ var app = new Vue({
         product: 'Socks',
         description: 'warm and cozy',
         image: './resources/img.jpg',
-        link: 'https://www.naver.com'
+        link: 'https://www.naver.com',
+        inventory: 100,
+        onSale: true,
+        details:["80% cotton", "20% polyster", "Gender-neutral "],
+        variants:[
+            {
+                variantId:2234,
+                variantColor: "green"
+            },
+            {
+                variantId:2235,
+                variantColor: "blue"
+            }
+        ],
+        sizeArr:[
+            {
+                size: "S",
+                length: 240
+            },
+            {
+                size: "M",
+                length: 260
+            },
+            {
+                size: "L",
+                length: 280
+            }
+        ],
+        cart: 0
     },
     functions:{
         sayhello(){}
