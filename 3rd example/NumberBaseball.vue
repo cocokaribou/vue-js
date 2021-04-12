56c7d32f5511ec33856ae9fda0225aee35df15d7
cocokaribou <younginlee2017@gmail.com>
2021-04-13 08:16:51 +0900

    0413

--- 3rd example/NumberBaseball.vue
+++ 3rd example/NumberBaseball.vue
@@ -7,7 +7,7 @@
       </form>
       <div>시도:{{attempts.length}}</div>
       <ul>
-          <li v-for="attempt in attempts">
+          <li v-for="attempt in attempts" :key="attempts.indexOf(attempt)">
               <div>{{attempt.count}}</div>
               <div>{{attempt.result}}</div>
         </li>
@@ -19,6 +19,7 @@
 export default {
     data(){
         return{
+            answer: getNumbers(),
             attempts:[], //입력한번 할때마다 push
             value:'',
             result: '',
@@ -26,14 +27,39 @@ export default {
     },
     methods: {
         onSubmitForm(){
-            this.attempts.push({
-                count: this.value,
-                result: 'home run!',
-            });
-            this.value ='';
-        }
+            if(this.value === this.answer.join('')){ //자료형 비교 주의!
+            //Array.prototype.join()
+            //배열의 요소를 문자열로 리턴
+                this.attempts.push({
+                    count: this.value,
+                    result: 'home run!',
+                });
+                alert('restart the game');
+                this.value ='';
+                this.attempts = [];
+            }else{
+                this.value ='';
+                this.value ='';
+            
+                
+            }
+        },
     },
 }
+
+const getNumbers = () =>{
+    console.error('will this be called')
+    const candidates = [1,2,3,4,5,6,7,8,9];
+    const array = [];
+    for(let i = 0; i<4; i++){
+        const chosen = candidates.splice(Math.floor(Math.random()*(9-i)), 1)[0];
+        array.push(chosen);
+
+        //Array.prototype.splice()
+        //배열의 기존 요소 삭제 또는 교체하거나 새 요소를 추가하여 배열의 내용을 변경
+    }
+    return array;
+};
 </script>
 
 <style>
